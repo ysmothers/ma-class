b5b73feed49f0a8923c9d90a5d5111863ab7de00
Ian McCarthy <ian.mccarthy@emory.edu>
2023-01-19 15:59:18 -0500

    add code files

--- data-code/1_Plan_Data.R
+++ data-code/1_Plan_Data.R
@@ -4,7 +4,8 @@
 
 for (y in 2007:2015) {
   ## Basic contract/plan information
-  contract.info=read_csv(paste0("data/input/monthly-ma-and-pdp-enrollment-by-cpsc/CPSC_Contract_Info_",y,"_01.csv"),
+  ma.path=paste0("data/input/monthly-ma-and-pdp-enrollment-by-cpsc/CPSC_Contract_Info_",y,"_01.csv")
+  contract.info=read_csv(ma.path,
                          skip=1,
                          col_names = c("contractid","planid","org_type","plan_type",
                                        "partd","snp","eghp","org_name","org_marketing_name",
@@ -23,40 +24,41 @@ for (y in 2007:2015) {
                            parent_org = col_character(),
                            contract_date = col_character()
                          ))
-  
+
   contract.info = contract.info %>%
     group_by(contractid, planid) %>%
     mutate(id_count=row_number())
-  
+    
   contract.info = contract.info %>%
     filter(id_count==1) %>%
     select(-id_count)
-  
-  ## Enrollments per plan
-  enroll.info=read_csv(paste0("data/input/monthly-ma-and-pdp-enrollment-by-cpsc/CPSC_Enrollment_Info_",y,"_01.csv"),
+    
+    ## Enrollments per plan
+  ma.path=paste0("data/input/monthly-ma-and-pdp-enrollment-by-cpsc/CPSC_Enrollment_Info_",y,"_01.csv")
+  enroll.info=read_csv(ma.path,
                        skip=1,
                        col_names = c("contractid","planid","ssa","fips","state","county","enrollment"),
                        col_types = cols(
-                         contractid = col_character(),
-                         planid = col_double(),
-                         ssa = col_double(),
-                         fips = col_double(),
-                         state = col_character(),
-                         county = col_character(),
-                         enrollment = col_double()
+                       contractid = col_character(),
+                       planid = col_double(),
+                       ssa = col_double(),
+                       fips = col_double(),
+                       state = col_character(),
+                       county = col_character(),
+                       enrollment = col_double()
                        ),na="*")
-  
-  
+    
+
   ## Merge contract info with enrollment info
   plan.data = contract.info %>%
     left_join(enroll.info, by=c("contractid", "planid")) %>%
     mutate(year=y)
-  
+    
   ## Fill in missing fips codes (by state and county)
   plan.data = plan.data %>%
     group_by(state, county) %>%
     fill(fips)
-  
+
   ## Fill in missing plan characteristics by contract and plan id
   plan.data = plan.data %>%
     group_by(contractid, planid) %>%
@@ -66,7 +68,7 @@ for (y in 2007:2015) {
   plan.data = plan.data %>%
     group_by(contractid) %>%
     fill(org_type,org_name,org_marketing_name,parent_org)
-  
+    
   ## Collapse from monthly data to yearly
   plan.year = plan.data %>%
     group_by(contractid, planid, fips) %>%
